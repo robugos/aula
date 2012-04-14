f5f58d7636962286a2802fca679446854044f258
Gustavopereira <Gustavopereira@hotmail.com.br>
2012-04-14 20:14:58 +0000

    First commit to Aula

---
+++ projetoaula/cadastro.py
@@ -0,0 +1,6 @@
+#---------------------------------------------------------Gustavo Pereira
+class cadastro:
+    def __init__(self, nome, departamento):
+        self.nome = nome
+        self.departamento = departamento
+#---------------------------------------------------------Gustavo Pereira
