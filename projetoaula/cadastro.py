dd480b5e5935aa9295f3f784368549b7f4e9b477
Gustavopereira <Gustavopereira@hotmail.com.br>
2012-04-17 19:53:33 +0000

    Cadastro de professores com departamento e disciplinas

--- projetoaula/cadastro.py
+++ projetoaula/cadastro.py
@@ -1,16 +1,32 @@
-#---------------------------------------------------------Gustavo Pereira
+#=======================================================Gustavo Pereira
 class cadastro:
-    def __init__(self, nome, departamento):
-        self.nome = nome
-        self.departamento = departamento
-#---------------------------------------------------------Gustavo Pereira
+    def __init__(self, nome, departamento,):
+        self.dados=[]
+        self.dados.append(nome+"\n")
+        self.dados.append(departamento+"\n")
 
-print 'teste'
-print "Teste Gustavo"
-print "teste robson"
-print "teste feijão"
-#Sera que vai funcionar
+    def cadastrar_disciplinas(self,disciplina):
+        self.dados.append(disciplina+" ")
 
-#Agora t‡ tudo funfando certinho.
+    def gravar(self):
+        SQL=open("Dados_Professores.txt","a")
+        SQL.writelines(self.dados)
+        SQL.close()
+        print "\nDados Atualizados no Banco de dados"
+        
+#===============================GRAVAR SQL==============================
+print "Professor, entre com seus dados.."
+nome=raw_input("Nome: ")
+departamento=raw_input("Departamento: ")
+professor=cadastro(nome,departamento)
 
-print "teste Rafaella"
+print "Entre com as disciplinas ministradas e para finalizar PRESS ENTER"
+disciplina=None
+while disciplina!='':
+    disciplina = raw_input("Disciplinas ministradas:")
+    if disciplina!='':
+        professor.cadastrar_disciplinas(disciplina)
+    else:
+        print "\nProfessor cadastrado com sucesso"
+professor.gravar()
+#=======================================================Gustavo Pereira
