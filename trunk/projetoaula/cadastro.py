5732f0636365764a7032a286522cf3d4c6b0e9e3
Gustavopereira@hotmail.com.br <Gustavopereira@hotmail.com.br@083044f6-e461-74a2-a8b3-a3afa1619da9>
2012-04-18 11:51:20 +0000

    Adição de CPF #1

--- trunk/projetoaula/cadastro.py
+++ trunk/projetoaula/cadastro.py
@@ -1,16 +1,18 @@
 #=======================================================Gustavo Pereira
 class cadastro:
-    def __init__(self, nome, departamento,):
+    def __init__(self, nome, departamento,cpf):
         self.dados=[]
         self.dados.append(nome+"\n")
         self.dados.append(departamento+"\n")
-
+        self.dados.append(cpf+"\n")
+        
     def cadastrar_disciplinas(self,disciplina):
         self.dados.append(disciplina+" ")
 
     def gravar(self):
         SQL=open("Dados_Professores.txt","a")
         SQL.writelines(self.dados)
+        SQL.write("\n\n")
         SQL.close()
         print "\nDados Atualizados no Banco de dados"
         
@@ -18,7 +20,8 @@ class cadastro:
 print "Professor, entre com seus dados.."
 nome=raw_input("Nome: ")
 departamento=raw_input("Departamento: ")
-professor=cadastro(nome,departamento)
+cpf=raw_input("CPF: ")
+professor=cadastro(nome,departamento,cpf)
 
 print "Entre com as disciplinas ministradas e para finalizar PRESS ENTER"
 disciplina=None
