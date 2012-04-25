2ef2acb84e956322e944fc9230e559f0f33dd50c
Gustavopereira <Gustavopereira@hotmail.com.br>
2012-04-25 01:06:21 +0000

    Classe Verificar CPF #ERRO na recursividade

---
+++ projetoaula/VerificarCPF.py
@@ -0,0 +1,37 @@
+# -*- coding: cp1252 -*-
+
+#===========Thiago, Robson, Rafaella, Mario e Gustavo===========#
+
+class Verificar():
+    def __init__(self,CPF):
+        self.userCPF = CPF
+        self.check=0
+        one=1
+        while self.check == 0:
+            if len(self.userCPF)==11:
+                try:
+                    for i in self.userCPF:
+                        if int(i) in [0,1,2,3,4,5,6,7,8,9]:
+                            continue
+                        else:
+                            break
+                except:
+                    print "CPF ERROR: (somente números)"
+                    self.userCPF = raw_input("CPF: ")
+                    Test = Verificar(self.userCPF)
+            else:
+                print "CPF ERROR: (somente 11 números)"
+                self.userCPF = raw_input("CPF: ")
+                Test = Verificar(self.userCPF)
+
+            self.check+=1
+        
+        else: 
+            print "CPF Válido" #ERRO printando recursivamente
+
+
+
+
+CPF = raw_input("Digite o CPF\n")
+Test = Verificar(CPF)
+#===========Thiago, Robson, Rafaella, Mario e Gustavo===========#
