ed44fdf3de583e705b58e5b421524d08815ac192
mario2545 <mario2545@gmail.com>
2012-05-09 14:01:56 +0000

    Arquivo DEFAULT para acesso no banco de dados. Toda alteração do banco de dados será feito nesse arquivo. by Gustavo e Mario

--- projetoaula/acesso_db.py
+++ projetoaula/acesso_db.py
@@ -1,42 +1,26 @@
-import MySQLdb
-
-from acesso_db import Servidor
-
 class Servidor():
-    def __init__ (self):
-        self.user = ""
-        self.password = ""
-        self.host = ""
-        self.banco = ""
+    def __init__(self):
+        self.user="yoshi"
+        self.password = "mario1234"
+        self.host = "aula.serveftp.com"
+        self.database = "AULADB"
 
-    def set_host(self,localhost):
-        self.host = localhost
-        
-    def set_user(self,usuario):
-        self.user = usuario
-        
-    def set_password(self,senha):
-        self.password = senha
-        
-    def set_banco(self,nomedb):
-        self.banco = nomedb
 
-#------------------DEFAULT        
-Serve = Servidor()
-Serve.set_host("7.0.6.8")
-Serve.set_user("yoshi")
-Serve.set_password("mario1234")
-Serve.set_banco(raw_input("AULADB")
-
+#    def set_host(self,localhost):
+#        self.host = localhost
+#        
+#    def set_user(self,usuario):
+#        self.user = usuario
+#        
+#    def set_password(self,senha):
+#        self.password = senha
+#        
+#    def set_banco(self,nomedb):
+#        self.banco = nomedb
+#
 #Serve.set_host(raw_input("Host: "))
 #Serve.set_user(raw_input("Usuario: "))
 #Serve.set_password(raw_input("Senha: "))
 #Serve.set_banco(raw_input("Nome Banco: "))
 
-db = MySQLdb.connect(Serve.host,Serve.user,Serve.password,Serve.banco) 
 
-#TESTESTESTESTES usaremos no outro arquivo
-#cursor=db.cursor()
-#sql="insert into DEPARTAMENTOS values( '0007','DEINFO','Giodano' ) "
-#cursor.execute(sql)
-#    
