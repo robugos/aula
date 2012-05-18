0b8b4fa73813e524b7171c3013c6595e39938d49
mario2545@gmail.com <mario2545@gmail.com@083044f6-e461-74a2-a8b3-a3afa1619da9>
2012-05-18 02:44:44 +0000

    Exclusão de Departameno 1.0

---
+++ trunk/projetoaula/exclusao_departamento.py
@@ -0,0 +1,33 @@
+# -*- coding: cp1252 -*-
+#===============================================================================
+from acesso_db import Servidor
+DATA = Servidor()
+#===============================================================================
+import MySQLdb
+db = MySQLdb.connect(DATA.host,DATA.user,DATA.password,DATA.database)
+cursor = db.cursor()
+#===============================================================================
+
+##OBS: Ainda falta informar se o departamento não existe, e não dá
+##pra excluir departamentos que tenham professores ou cursos cadastrados neles,
+##ou seja, só dá pra excluir um departamento se ele tiver totalmente vazio.
+##precisamos corrigir isso.
+
+def excluirDepartamento(cursor, deletado):
+    sql = "delete from departamentos where id_departamento='%s'" %(deletado)
+    try:
+        cursor.execute(sql)
+        db.commit()
+        print "Departamento excluido com sucesso."
+    except:
+        print "Erro na exclusão, há professores ou cursos atrelados ao departamento."
+        db.rollback()
+
+print "------ EXCLUSÃO DE DEPARTAMENTO ------"
+saida = None
+while saida <> "s":
+    deletado = raw_input('Digite o id do departamento que deseja excluir: ')
+    excluirDepartamento(cursor, deletado)
+    saida = raw_input('Digite s para sair ou enter para continuar excluindo: ')
+print "------ FINISH ------"
+db.close()
