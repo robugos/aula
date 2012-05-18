832179beb3dd5db562945150babd9956b3f85875
mario2545 <mario2545@gmail.com>
2012-05-18 15:05:46 +0000

    Cadastro CURSO

---
+++ projetoaula/cadastro_curso.py
@@ -0,0 +1,38 @@
+# -*- coding: cp1252 -*-
+#===============================================================================
+from acesso_db import Servidor
+DATA = Servidor()
+#===============================================================================
+import MySQLdb
+db = MySQLdb.connect(DATA.host,DATA.user,DATA.password,DATA.database)
+cursor = db.cursor()
+#===============================================================================
+
+def cadastrarCurso(cursor, id_curso, nome_curso, departamento_curso):
+    sql = "insert into cursos values('%s','%s','%s')"%(id_curso, nome_curso, departamento_curso)
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        print "Erro no cadastro. Por favor verifique se os campos foram inseridos corretamente."
+        db.rollback()
+        
+#===============================================================================
+        
+print "------ CADASTRO DE CURSOS ------"
+saida = None
+while saida <> "s":
+    id_curso = raw_input("Digite o ID do curso: ")
+    verificar_curso = "select id_curso from cursos where id_curso='%s'" %(id_curso)
+    existe = cursor.execute(verificar_curso)
+    if existe > 0:
+        print "Curso já existe."
+        continue
+    else:
+        nome_curso = raw_input("Digite o nome do curso: ")
+        departamento_curso = raw_input("Digite o departamento do curso: ")
+        cadastrarCurso(cursor, id_curso, nome_curso, departamento_curso)
+        saida = raw_input('Digite s para sair ou enter pra continuar: ')
+    
+print "------ CADASTRO EFETUADO COM SUCESSO ------"
+db.close()
