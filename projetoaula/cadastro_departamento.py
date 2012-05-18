10013a876c9ec5a9bc94d4b0e5e1998d8dc5af71
mario2545 <mario2545@gmail.com>
2012-05-18 12:30:22 +0000

    update 1

---
+++ projetoaula/cadastro_departamento.py
@@ -0,0 +1,29 @@
+# -*- coding: cp1252 -*-
+#===============================================================================
+from acesso_db import Servidor
+DATA = Servidor()
+#===============================================================================
+import MySQLdb
+db = MySQLdb.connect(DATA.host,DATA.user,DATA.password,DATA.database)
+cursor = db.cursor()
+#===============================================================================
+
+def cadastroDepartamento(cursor, id_departamento, nome, coordenador):
+    sql = "insert into departamentos values('%s','%s','%s')" %(id_departamento, nome, coordenador)
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        print "Erro no cadastro. Por favor verifique se os campos foram inseridos corretamente."
+        db.rollback()
+
+print "------ CADASTRO DE DEPARTAMENTO ------"
+saida = None
+while saida <> "s":
+    id_departamento = raw_input('Digite o id do departamento: ')
+    nome = raw_input("Digite o nome do departamento: ")
+    coordenador = raw_input("Digite o nome do coordenador: ")
+    cadastroDepartamento(cursor, id_departamento, nome, coordenador)
+    saida = raw_input('Digite s para sair ou enter pra continuar: ')
+print "------ FINISH ------"
+db.close()
