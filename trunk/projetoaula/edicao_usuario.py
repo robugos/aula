729af5b7eb7e7bc3812c594cf9637d619736d2de
mario2545@gmail.com <mario2545@gmail.com@083044f6-e461-74a2-a8b3-a3afa1619da9>
2012-05-22 13:14:00 +0000

    Edição de Usuário 1.0

---
+++ trunk/projetoaula/edicao_usuario.py
@@ -0,0 +1,69 @@
+# -*- coding: cp1252 -*-
+#===============================================================================
+from acesso_db import Servidor
+DATA = Servidor()
+#===============================================================================
+import MySQLdb
+db = MySQLdb.connect(DATA.host,DATA.user,DATA.password,DATA.database)
+cursor = db.cursor()
+#===============================================================================
+
+def editarCPF(cursor, id_novo, id_CPF):
+    sql = "update usuarios set usuario_cpf='%s' where usuario_CPF='%s'" %(id_novo, id_CPF)
+    try:
+        cursor.execute(sql)
+        db.commit()
+        print "CPF editado com sucesso."
+    except:
+        print "Erro na edição. Por favor verifique se os campos foram inseridos corretamente."
+        db.rollback()
+        
+def editarSenha(cursor, senha, id_CPF):
+    sql = "update usuarios set senha='%s' where usuario_cpf='%s'" %(senha, id_CPF)
+    try:
+        cursor.execute(sql)
+        db.commit()
+        print "Senha editado com sucesso."
+    except:
+        print "Erro na edição. Por favor verifique se os campos foram inseridos corretamente."
+        db.rollback()
+        
+def editarClasse(cursor, classe, id_CPF):
+    sql = "update usuarios set classe='%d' where usuario_cpf='%s'" %(classe, id_CPF)
+    try:
+        cursor.execute(sql)
+        db.commit()
+        print "Classe editado com sucesso."
+    except:
+        print "Erro na edição. Por favor verifique se os campos foram inseridos corretamente."
+        db.rollback()
+        
+#===============================================================================
+
+print "------ EDIÇÃO DE USUARIO ------"
+saida = None
+while saida <> "s":
+    id_CPF = raw_input('Digite o CPF do usuario que deseja editar: ')
+    verificar_user = "select usuario_cpf from usuarios where usuario_cpf='%s'" %(id_CPF)
+    existe = cursor.execute(verificar_user)
+    if existe < 1:
+        print "Usuario não existente."
+        continue
+    else:
+        opcao = input("Digite 1 (editar CPF), 2 (editar senha) ou 3 (editar classe): ")
+        if opcao > 3 or opcao < 1:
+            print "Opção incorreta."
+            continue
+        else:
+            if opcao == 1:
+                id_novo = raw_input("Digite o novo CPF do usuario: ")
+                editarCPF(cursor, id_novo, id_CPF)
+            if opcao == 2:
+                senha = raw_input("Digite a nova senha do usuario: ")
+                editarSenha(cursor, senha, id_CPF)
+            if opcao == 3:
+                classe = input("Digite o valor da nova classe (0 ou 1): ")
+                editarClasse(cursor, classe, id_CPF)
+            saida = raw_input('Digite s para sair ou enter pra continuar: ')
+print "------ FINISH ------"
+db.close()
