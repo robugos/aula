19527867251e9da99ddff4ee60500ab564a5a53c
mario2545@gmail.com <mario2545@gmail.com@083044f6-e461-74a2-a8b3-a3afa1619da9>
2012-05-23 12:35:06 +0000

    Login 2.0

--- trunk/projetoaula/login.py
+++ trunk/projetoaula/login.py
@@ -5,6 +5,7 @@ DATA = Servidor()
 #===============================================================================
 import MySQLdb
 db = MySQLdb.connect(DATA.host,DATA.user,DATA.password,DATA.database)
+cursor = db.cursor()
 #===============================================================================
 from Validacao import Validacao
 #===============================================================================
@@ -20,20 +21,31 @@ class Login_professor():
     def userSenha(self, password): #Precisa fazer o check com o Banco de dados, ainda
         self.password = password
         
-    def imprimir_dados(self):
+    def acessar_paginas(self):
         print self.password, self.profCPF
         
 #=======================================TESTES==================================
 
 print "          << LOGIN >>"
-CPF = raw_input("CPF: ")
-Test = Validacao()
-CPF = Test.CPF_Check(CPF)
 
 Login = Login_professor()
-Login.prof_CPF(CPF)
 
-password = raw_input("Senha: ")
-Login.userSenha(password)
-
-Login.imprimir_dados()
+cpf = raw_input("Digite o login (CPF): ")
+verificar_CPF = "select usuario_cpf from usuarios where usuario_cpf='%s'" %(cpf)
+existe = cursor.execute(verificar_CPF)
+if existe < 1:
+    print "Usuario não existente."
+else:
+    Login.prof_CPF(cpf)
+    acesso = False
+    while acesso == False:
+        senha = raw_input("Digite a senha: ")
+        verificar_senha = "select usuario_cpf='%s' from usuarios where senha='%s'" %(cpf,senha)
+        executar = cursor.execute(verificar_senha)
+        if executar < 1:
+            print "Senha incorreta."
+        else:
+            acesso = True
+            Login.userSenha(senha)
+    if acesso == True:
+        Login.acessar_paginas()
