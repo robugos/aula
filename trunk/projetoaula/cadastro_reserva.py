a8c696e90bc3dd6bb48468aba03bead2af3ed0f9
Gustavopereira@hotmail.com.br <Gustavopereira@hotmail.com.br@083044f6-e461-74a2-a8b3-a3afa1619da9>
2012-06-05 03:12:44 +0000

    100%

--- trunk/projetoaula/cadastro_reserva.py
+++ trunk/projetoaula/cadastro_reserva.py
@@ -23,21 +23,27 @@ def cadastrarReserva(cursor, predio_reserva, local_reserva, data, disciplina_res
             print "Erro no reserva."
             db.rollback()
 
-#===============================================================================
+def print_predios():
+    cursor.execute("select * from predios")
+    lista_predios_ID=[]
+    lista_predios_Nome=[]
+    for row in cursor.fetchall():
+        lista_predios_ID.append(row[0])
+        lista_predios_Nome.append(row[1])
+    print "  ID      NOME"
+    for i in range (len(lista_predios_ID)):
+        print "["+lista_predios_ID[i]+"] - "+lista_predios_Nome[i]
         
-print "------ CADASTRO DE RESERVAS ------"
-saida = None
-while saida <> "s":
-    Test = Validacao()
-    predio_reserva = raw_input("Digite o prédio da reserva ")
-    local_reserva = raw_input("Digite o local da reserva: ")
-    data = raw_input("Digite a data da reserva (0000-00-00) ")
-    disciplina_reserva = raw_input("Digite a disciplina da reserva: ")
-    professor_reserva = raw_input("Digite o professo da reserva: ")
-    hora1 = input("Digite horário de inicio: ")
-    hora2 = input("Digite horário de fim: ")
-    cadastrarReserva(cursor, predio_reserva, local_reserva, data, disciplina_reserva, professor_reserva, hora1, hora2)
-    saida = raw_input('Digite s para sair ou enter pra continuar: ')
-    
-print "------ CADASTRO EFETUADO COM SUCESSO ------"
-db.close()
+def print_local(predio_reserva):
+    cursor.execute("select * from locais where predio_local='%s'" %(predio_reserva))
+    lista_locais_ID=[]
+    lista_locais_Nome=[]
+    lista_locais_Tipo=[]
+    for row in cursor.fetchall():
+        lista_locais_ID.append(row[0])
+        lista_locais_Nome.append(row[1])
+        lista_locais_Tipo.append(row[2])
+    print " ID     NOME     TIPO"
+    for i in range (len(lista_locais_ID)):
+        print "["+lista_locais_ID[i]+"] - "+lista_locais_Nome[i]+" - "+lista_locais_Tipo[i]
+#===============================================================================
