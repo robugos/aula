47050232a31ee13d49d3e10ecf99b32f052c88f1
Gustavopereira <Gustavopereira@hotmail.com.br>
2012-06-05 03:10:23 +0000

    Login modificado

--- projetoaula/login.py
+++ projetoaula/login.py
@@ -18,31 +18,4 @@ class Login_professor():
         
     def userSenha(self, password):
         self.password = password
-        
-        
-#===============================================================================
-
-#Login = Login_professor()
-#end=0
-#while end == 0:
-#    cpf = raw_input("Digite o login (CPF): ")
-#    verificar_CPF = "select usuario_cpf from usuarios where usuario_cpf='%s'" %(cpf)
-#    existe = cursor.execute(verificar_CPF)
-#    if existe < 1:
-#        print "Usuario não existente."
-#        continue
-#    else:
-#        Login.prof_CPF(cpf)
-#        acesso = False
-#        while acesso == False:
-#            senha = raw_input("Digite a senha: ")
-#            verificar_senha = "select usuario_cpf='%s' from usuarios where senha='%s'" %(cpf,senha)
-#            executar = cursor.execute(verificar_senha)
-#            if executar < 1:
-#                print "Senha incorreta."
-#            else:
-#                acesso = True
-#                Login.userSenha(senha)
-#        if acesso == True:
-#            Login.acessar_paginas()
-#        end=1
+  
