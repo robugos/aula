67368667e8044a45dc055d982eef4d1a3972c7af
Gustavopereira@hotmail.com.br <Gustavopereira@hotmail.com.br@083044f6-e461-74a2-a8b3-a3afa1619da9>
2012-06-05 03:10:57 +0000

    Exclusao reserva modificado 50%

--- trunk/projetoaula/exclusao_reserva.py
+++ trunk/projetoaula/exclusao_reserva.py
@@ -20,28 +20,28 @@ def excluirReserva(cursor, deletado):
         
 #===============================================================================
 
-print "------ EXCLUSÃO DE RESERVA ------"
-saida = None
-while saida <> "s": 
-    data = raw_input("Digite a data da reserva (0000-00-00) ")
-    predio_reserva = raw_input("Digite o prédio da reserva ")
-    local_reserva = raw_input("Digite o local da reserva: ")
-    hora1 = input("Digite horário de inicio: ")
-    hora2 = input("Digite horário de fim: ")
-    
-    for horario in range (hora1,hora2,1):
-        hora = str(horario)
-        id_reserva = hora+"_"+data+"_"+predio_reserva+"_"+local_reserva
-    
-        verificar_reserva = "select id_reserva from reservas where id_reserva='%s'" %(id_reserva)
-        existe = cursor.execute(verificar_reserva)
-    
-        if existe < 1:
-            print "Reserva não existente no horário das %s horas" %(hora)
-            continue
-        else:
-            excluirReserva(cursor, id_reserva)
-    saida = raw_input('Digite s para sair ou enter para continuar excluindo: ')
-            
-print "------ FINISH ------"
-db.close()
+#print "------ EXCLUSÃO DE RESERVA ------"
+#saida = None
+#while saida <> "s": 
+#    data = raw_input("Digite a data da reserva (0000-00-00) ")
+#    predio_reserva = raw_input("Digite o prédio da reserva ")
+#    local_reserva = raw_input("Digite o local da reserva: ")
+#    hora1 = input("Digite horário de inicio: ")
+#    hora2 = input("Digite horário de fim: ")
+#    
+#    for horario in range (hora1,hora2,1):
+#        hora = str(horario)
+#        id_reserva = hora+"_"+data+"_"+predio_reserva+"_"+local_reserva
+#    
+#        verificar_reserva = "select id_reserva from reservas where id_reserva='%s'" %(id_reserva)
+#        existe = cursor.execute(verificar_reserva)
+#    
+#        if existe < 1:
+#            print "Reserva não existente no horário das %s horas" %(hora)
+#            continue
+#        else:
+#            excluirReserva(cursor, id_reserva)
+#    saida = raw_input('Digite s para sair ou enter para continuar excluindo: ')
+#            
+#print "------ FINISH ------"
+#db.close()
